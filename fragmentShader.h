593bd3a0e722b67bb238e27378f0e7c6a71e17e0
Sergio Moura <sergio@moura.us>
2014-08-07 13:10:54 -0300

    Properly setting the alpha value on fragment shader

--- fragmentShader.h
+++ fragmentShader.h
@@ -25,14 +25,14 @@ const char fragmentShader[] = "#version 330 core\n\
 in vec2 UV;\n\
 \n\
 // Ouput data\n\
-out vec3 color;\n\
+out vec4 color;\n\
 \n\
 // Values that stay constant for the whole mesh.\n\
 uniform sampler2D myTextureSampler;\n\
 \n\
 void main() {\n\
     // Output color = color of the texture at the specified UV\n\
-    color = texture(myTextureSampler, UV).rrr;\n\
+    color = texture(myTextureSampler, UV).rrrr;\n\
 }";
 
 #endif /* __FONTSTASH_FRAGMENTSHADER_H */
