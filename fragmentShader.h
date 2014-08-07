6451efe09bae5e932bce257a9984e66cb28d5609
Sergio Moura <sergio@moura.us>
2014-08-07 08:18:38 -0300

    fix fragment shader

--- fragmentShader.h
+++ fragmentShader.h
@@ -2,15 +2,19 @@
 #define __FONTSTASH_FRAGMENTSHADER_H
 
 const char fragmentShader[] = "#version 330 core\n\
+\n\
 // Interpolated values from the vertex shaders\n\
 in vec2 UV;\n\
+\n\
 // Ouput data\n\
 out vec3 color;\n\
+\n\
 // Values that stay constant for the whole mesh.\n\
 uniform sampler2D myTextureSampler;\n\
+\n\
 void main() {\n\
     // Output color = color of the texture at the specified UV\n\
-    color = texture2D( myTextureSampler, UV ).rgb;\n\
+    color = texture(myTextureSampler, UV).rgb;\n\
 }";
 
 #endif /* __FONTSTASH_FRAGMENTSHADER_H */
